7e25930d682edcc6bf0b5615e26589a52e23ab6f
Varrel Al Jabaar Konstitusi <102298294+triliun@users.noreply.github.com>
2024-04-01 22:34:08 +0700

    FIX SOME BUG

--- app/View/DashboardView.php
+++ app/View/DashboardView.php
@@ -6,26 +6,70 @@
   <title>Dahsboard - Akunime</title>
 </head>
 <body>
-<div class="nav-flex">
-    
+
+
+<header class="nav-flex">
 <nav id="sidebar" class="sidebar d-none">
     <div class="side-head">
     <h2 class="title">Akunime</h2>
     <i id="close"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg></i>
     </div>
     <div class="side-link">
-    <a href="">Dashboard</a>
-    <a href="">Dashboard</a>
-    <a href="">Dashboard</a>
+    <a href="" class="active">Dashboard</a>
+    <a href="">Post Anime</a>
+    <a href="">Post Video</a>
     </div>
 </nav>
+
+<div class="w-full">
 <nav class="navbar">
     <div class="nav-head">
     <i id="open"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg></i>
-        <h2 class="title">Dashboard</h2>
+    <i class='search'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path><path d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path></svg></i>
+    </div>
+    <div class="nav-right">
+    <i><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13.586V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v3.586l-1.707 1.707A.996.996 0 0 0 3 16v2a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L19 13.586zM19 17H5v-.586l1.707-1.707A.996.996 0 0 0 7 14v-4c0-2.757 2.243-5 5-5s5 2.243 5 5v4c0 .266.105.52.293.707L19 16.414V17zm-7 5a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22z"></path></svg></i>
+        <img src="goblin-slayer.jpg" width="24" height="24" class="profile">
     </div>
 </nav>
+<main>
+    <!-- Bagiaaman Kita menulismembuat templat di sini? seperti {{ slot }} -->
+    <h2 class="title">Dashboard</h2>
+
+    <div class="card-main">
+    <div class="card">
+        <div>
+            <h3>1021+</h3>
+            <span>User</span>
+        </div>
+        <i><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></i>
+    </div>
+    <div class="card">
+        <div>
+            <h3>64K+</h3>
+            <span>View</span>
+        </div>
+        <i><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></i>
+    </div>
+    <div class="card">
+        <div>
+            <h3>64</h3>
+            <span>Anime</span>
+        </div>
+        <i><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></i>
+    </div>
+    <div class="card">
+        <div>
+            <h3>223</h3>
+            <span>Video</span>
+        </div>
+        <i><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></i>
+    </div>
+        
+    </div>
+</main>
 </div>
+</header>
 
 
 <style type="text/css">
@@ -59,12 +103,51 @@ body {
     background-color: var(--body-color);
 }
 
+.d-flex {
+    display: flex;
+}
+
+.w-full {
+    width: 100%;
+}
+
+main {
+    padding: 10px 10px;
+}
+
+.card-main {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 5px;
+    margin-top: 10px;
+}
+
+.card {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    color: var(--text-color);
+    background-color: var(--search-bar);
+    border-radius: 10px;
+    padding: 3px 10px;
+        width: calc(100% / 4 - 10px);
+/*    width: 200px;*/
+    height: 90px;
+}
+
 .nav-flex {
     display: flex;
 }
 
+.sidebar {
+    width: 210px;
+    height: 100%;
+/*    border-right: 1px solid var(--text-color);*/
+    background-color: var(--body-color);
+}
 .side-head {
-    padding: 3px 5px;
+    padding: 3px 10px;
     display: flex;
     justify-content: space-between;
 }
@@ -78,16 +161,7 @@ svg {
     font-weight: bold;
 }
 
-.sidebar {
-    width: 200px;
-    height: 100%;
-    border-right: 1px solid var(--text-color);
-    background-color: var(--body-color);
-}
 
-.side-link {
-    margin: 15px 0;
-}
 
 .side-link a {
     appearance: none;
@@ -96,11 +170,50 @@ svg {
     font-weight: 800;
     width: 100%;
     padding: 8px 54px;
+    text-align: center;
     display: block;
+    white-space: nowrap;
     &:hover {
     background-color: var(--search-bar);
 
     }
+}
+
+.side-link .active {
+    background-color: var(--search-bar);
+    pointer-events: none;
+}
+
+
+/* NAVBAR */
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 3px 10px;
+    background-color: var(--search-bar);
+    width: 100%;
+    height: 42px;
+}
+
+.nav-head {
+    display: flex;
+    gap: 5px;
+}
+
+.nav-right {
+    display: flex;
+    gap: 5px;
+}
+
+.profile {
+    border-radius: 100%;
+    display: block;
+    margin-top: 5px;
+    object-fit: cover;
+    vertical-align: middle;
+    justify-items: center;
+    width: 24px;
+    height: 24px;
 }
 
  /* MEDIA QUERY FOR SIDEBAR */
@@ -133,19 +246,16 @@ svg {
         }
     }
 
-/* NAVBAR */
-.navbar {
-    padding: 3px 5px;
-    background-color: var(--search-bar);
-    width: 100%;
-    height: 41px;
+@media (max-width: 660px) {
+    .card {
+        width: calc(100% / 2 - 10px);
+    }
 }
-
-.nav-head {
-    display: flex;
-    gap: 5px;
+@media (max-width: 360px) {
+    .card {
+        width: calc(100% / 1 - 10px);
+    }
 }
-
 
 
 </style>
